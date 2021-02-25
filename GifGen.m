279a01d30617a11c4cbdeb1d41ed156cd38f9bae
EdDarnbrough <51373340+EdDarnbrough@users.noreply.github.com>
2021-02-25 11:28:10 +0000

    Create GifGen.m

---
+++ GifGen.m
@@ -0,0 +1,23 @@
+h = figure;
+axis tight manual % this ensures that getframe() returns a consistent size
+filename = 'testAnimated.gif';
+var = var.Test;
+for n = 1:2626 %length(var)
+    % Draw plot for y = x.^n
+    t = var(1:n,2)*10^-9;
+    d = var(1:n,3)*10^-3;
+    plot(t,d, 'r')
+    axis([0 6e-5 0 25])
+    xlabel('Displacement (m)'); ylabel('Load (\muN)') 
+    drawnow 
+      % Capture the plot as an image 
+      frame = getframe(h); 
+      im = frame2im(frame); 
+      [imind,cm] = rgb2ind(im,256); 
+      % Write to the GIF File 
+      if n == 1 
+          imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+      else 
+          imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+      end 
+  end
